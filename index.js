0194e2bd0c1b3e0ae71d67dd16130932a548e2ba
Eric Kelvin Waweru <kelsric@yahoo.com>
2022-06-11 08:48:10 +0300

    commit tree e2d2dead3b350519591631b1d6d69352089a6e71

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log (multiply);
+
+/* const random = math.floor(
+   math.random()*100 
+) +1;
+console.log(random);*/
+
+const random = Math.floor(Math.random() * 100) + 1; 
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+
+const max = Math.max(5,20,10,4);
